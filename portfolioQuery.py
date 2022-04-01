840109951fc6f0892b217ce719b8fac7a8d2dc91
Nicholas Karch <47068567+nickarch511@users.noreply.github.com>
2022-04-01 10:01:15 -0400

    Add main class (Portfolio), inside PortfolioQuery file

---
+++ portfolioQuery.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pandas as pd
+import yfinance as yf
+
+"""
+This class will control a given stock portfolio.
+It will contain two instance variables, self.stocks and self.numstocks
+which contain the strings of all valid tickers and the number of 
+valid tickers in the portfolio.
+
+Any ticker that is invalid that is passed into the constructor will not
+be added to the Portfolio object's stock_list or num_stocks instance variables.
+
+"""
+class Portfolio:
+
+    # Constructor
+    def __init__(self, stock_list):
+        
+        self.stocks = []
+
+        for stock in stock_list:
+            ticker = yt.Ticker(stock)
+            
+            # Determine whether is it a valid ticker
+            try:
+                info = ticker.info # If this runs, it is valid
+                self.stocks.append(stock)
+            except: 
+                continue # invalid
+        self.num_stocks = len(self.stocks)
+    
+    def add_stocks(self, stock_list):
+        for stock in stock_list:
+            ticker = yt.Ticker(stock)
+            
+            # Determine whether is it a valid ticker
+            try:
+                info = ticker.info # If this runs, it is valid
+                self.stocks.append(stock)
+                self.num_stocks += 1
+            except: 
+                continue # invalid
+        
+    def clear_stocks(self, stock_list):
+        self.stocks = []
+        self.num_stocks = 0
+
+    def getTangentPortfolio(self):
+        return [0 for i in range(self.num_stocks)]
+
+    def getLongPortfolio(self):
+        return [0 for i in range(self.num_stocks)]
+
+    def getLimitedLeveragePortfolio(self):
+        return [0 for i in range(self.num_stocks)]
