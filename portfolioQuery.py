045276860290c30bc03e6c17a5ecc6cc54199e09
Nicholas Karch <47068567+nickarch511@users.noreply.github.com>
2022-04-07 11:48:24 -0400

    Add function to select stocks as opposed to requiring user input. To be implemented in GUI next (graphical.py)

--- portfolioQuery.py
+++ portfolioQuery.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import yfinance as yf
-import dataframe_image as dfi
 import requests
 from bs4 import BeautifulSoup
+import random
 
 
 
@@ -213,6 +213,57 @@ class Portfolio:
         plt.show()
 
 
+    '''
+    This is a helper function for pickLongPortfolio
+    The asset string here will be a space-delimited string
+    of all ticker symbols.
+
+    The Rf will be the coupon rate for a 52-week treasury bill
+
+    returns flt object and names
+    '''
+    def getLongPortfolioGivenAssetStringAndRf(self, asset_names, muSi, money_to_invest):
+        a_year = timedelta(days=365)
+        datetime.strftime(datetime.today() - a_year, "%Y-%m-%d")
+        data = data = yf.download(" ".join(asset_names), start=datetime.strftime(datetime.today() - a_year, "%Y-%m-%d"), end=datetime.strftime(datetime.today(), "%Y-%m-%d"))
+        asset_data = {}
+        for name in asset_names:
+            x = pd.DataFrame(data.xs(name,axis=1, level=1))
+            x["Date"] = x.index
+            asset_data[name] = fm.getDailyReturnsDataFrame(x)
+        muSi = (1+muSi)**(1/len(asset_data[asset_names[0]]['Date'])) - 1
+        M = fm.getM(asset_data, asset_names, "1999-01-01", "2023-01-01")
+        V = fm.getV(asset_data, asset_names, "2021-01-01", "2022-01-01")
+        flt = fm.getFlt(V,M,muSi)
+        print("The expected risk of this portfolio is {}, and the expected return is {}".format(flt['sigma'], flt['mu']))
+        for i,name in zip(flt['Flt'][0]*money_to_invest, asset_names):
+            if i > 500: print("{}: {}".format(name,i))
+        return flt, asset_names
+
+
+    '''
+    While other features of this class require a portfolio, this function will 
+    select the best portfolio of size numAssets that it finds after 100 random
+    portfolios.
+    It maximizes the ratio of reward to return
+    '''
+    def pickLongPortfolio(self, numAssets):
+        assets = x = "ZTS ZION ZBRA ZBH YUM XYL XRAY XOM XLNX XEL WYNN WY WU WST WRK WRB WMT WMB WM WHR WFC WELL WEC WDC WBA WAT WAB VZ VTRS VTR VRTX VRSN VRSK VNO VMC VLO VIAC VFC V USB URI UPS UNP UNH ULTA UHS UDR UAL UAA UA TYL TXT TXN TWTR TTWO TT TSN TSLA TSCO TRV TROW TRMB TPR TMUS TMO TJX TGT TFX TFC TER TEL TECH TDY TDG TAP T SYY SYK SYF SWKS SWK STZ STX STT STE SRE SPGI SPG SO SNPS SNA SLB SJM SIVB SHW SEE SCHW SBUX SBAC RTX RSG ROST ROP ROL ROK RMD RL RJF RHI RF REGN REG RE RCL QRVO QCOM PYPL PXD PWR PVH PTC PSX PSA PRU PPL PPG POOL PNW PNR PNC PM PLD PKI PKG PHM PH PGR PG PFG PFE PEP PENN PEG PEAK PCAR PBCT PAYX PAYC OXY OTIS ORLY ORCL OMC OKE OGN ODFL O NXPI NWSA NWS NWL NVR NVDA NUE NTRS NTAP NSC NRG NOW NOC NLSN NLOK NKE NI NFLX NEM NEE NDAQ NCLH MU MTD MTCH MTB MSI MSFT MSCI MS MRO MRNA MRK MPC MOS MO MNST MMM MMC MLM MKTX MKC MHK MGM MET MDT MDLZ MCO MCK MCHP MCD MAS MAR MAA MA LYV LYB LW LVS LUV LUMN LRCX LOW LNT LNC LMT LLY LKQ LIN LHX LH LEN LEG LDOS L KR KO KMX KMI KMB KLAC KIM KHC KEYS KEY K JPM JNPR JNJ JKHY JCI JBHT J IVZ ITW IT ISRG IRM IR IQV IPGP IPG IP INTU INTC INFO INCY ILMN IFF IEX IDXX ICE IBM HWM HUM HSY HST HSIC HRL HPQ HPE HON HOLX HLT HII HIG HES HD HCA HBI HBAN HAS HAL GWW GS GRMN GPS GPN GPC GOOGL GOOG GNRC GM GLW GL GIS GILD GE GD FTV FTNT FRT FRC FOXA FOX FMC FLT FITB FISV FIS FFIV FE FDX FCX FBHS FB FAST FANG F EXR EXPE EXPD EXC EW EVRG ETSY ETR ETN ESS ES EQR EQIX EOG ENPH EMR EMN EL EIX EFX ED ECL EBAY EA DXCM DXC DVN DVA DUK DTE DRI DRE DPZ DOW DOV DLTR DLR DISH DISCK DISCA DIS DHR DHI DGX DG DFS DE DD D CZR CVX CVS CTXS CTVA CTSH CTRA CTLT CTAS CSX CSCO CRM CRL CPRT CPB COST COP COO COF CNP CNC CMS CMI CMG CME CMCSA CMA CLX CL CINF CI CHTR CHRW CHD CFG CF CERN CE CDW CDNS CDAY CCL CCI CBRE CBOE CB CAT CARR CAH CAG C BXP BWA BSX BRO BR BMY BLL BLK BKR BKNG BK BIO BIIB  BEN BDX BBY BBWI BAX BAC BA AZO AXP AWK AVY AVGO AVB ATVI ATO ARE APTV APH APD APA AOS AON ANTM ANSS ANET AMZN AMT AMP AMGN AME AMD AMCR AMAT ALLE ALL ALK ALGN ALB AKAM AJG AIZ AIG AFL AES AEP AEE ADSK ADP ADM ADI ADBE ACN ABT ABMD ABC ABBV AAPL AAP AAL A".split(" ")
+        x = [i for i in x if i != '']
+        bestflt = None
+        bestNames = None
+        bestRewardToReturnRatio = 0
+
+        for i in range(100):
+            flt,names = self.getLongPortfolioGivenAssetStringAndRf(random.sample(x,numAssets), self.get_si_rate(), 1)
+            if flt['mu']/flt['sigma'] > bestRewardToReturnRatio:
+                bestRewardToReturnRatio = flt['mu']/flt['sigma']
+                bestflt = flt
+                bestNames = names
+        print(bestflt)
+        print(bestNames)
+        self.stocks = bestNames
+        return bestflt
 
     # Add string method
     def __str__(self):
